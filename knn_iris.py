59d4461656fdc5e21840c8e272a6c8d64d8d9508
Yaseen Lotfi <yalotfi@outlook.com>
2017-02-04 22:15:07 -0500

    clear pep8 format issues

--- knn_iris.py
+++ knn_iris.py
@@ -36,7 +36,7 @@ def process_data():
     iris = datasets.load_iris()
     X = iris.data
     y = iris.target
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.5)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
     return X_train, X_test, y_train, y_test
 
 
