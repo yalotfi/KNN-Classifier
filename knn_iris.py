1078281251f75569ca765ef87fccb5e72f13d61b
Yaseen Lotfi <yalotfi@outlook.com>
2017-02-04 22:07:06 -0500

    clean up and formalize algorithm

--- knn_iris.py
+++ knn_iris.py
@@ -1,22 +1,19 @@
+from scipy.spatial import distance
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
-from scipy.spatial import distance
 from sklearn import datasets
 
 
-def euc(a, b):
-    return distance.euclidean(a, b)
-
-
 class scrappy_knn():
-    def fit(self, X_train, y_train):
+    def fit(self, X_train, y_train, k):
         self.X_train = X_train
         self.y_train = y_train
+        self.k = k
 
-    def closest(self, row):
+    def closest(self, row, k):
         best_dist = euc(row, self.X_train[0])
         best_index = 0
-        for i in range(1, len(self.X_train)):
+        for i in range(self.k, len(self.X_train)):
             dist = euc(row, self.X_train[i])
             if dist < best_dist:
                 best_dist = dist
@@ -26,19 +23,30 @@ class scrappy_knn():
     def predict(self, X_test):
         predictions = []
         for row in X_test:
-            label = self.closest(row)
+            label = self.closest(row, 1)
             predictions.append(label)
         return predictions
 
 
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.5)
+def euc(a, b):
+    return distance.euclidean(a, b)
 
 
-classifier = scrappy_knn()
-classifier.fit(X_train, y_train)
-predictions = classifier.predict(X_test)
+def process_data():
+    iris = datasets.load_iris()
+    X = iris.data
+    y = iris.target
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.5)
+    return X_train, X_test, y_train, y_test
 
-print(accuracy_score(y_test, predictions))
+
+def knn(k_nieghbor):
+    X_train, X_test, y_train, y_test = process_data()
+    classifier = scrappy_knn()
+    classifier.fit(X_train, y_train, k_nieghbor)
+    predictions = classifier.predict(X_test)
+    print(accuracy_score(y_test, predictions))
+
+
+if __name__ == '__main__':
+    knn(7)
