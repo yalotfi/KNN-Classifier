236fca1dee6c3eea24277b7c87b6565813a3dc87
Yaseen Lotfi <yalotfi@outlook.com>
2017-02-05 15:01:38 -0500

    use numpy instead of spatial dependency

--- knn_iris.py
+++ knn_iris.py
@@ -1,4 +1,4 @@
-from scipy.spatial import distance
+import numpy as np
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn import datasets
@@ -10,32 +10,32 @@ class scrappy_knn():
         self.y_train = y_train
         self.k = k
 
+    def euc(self, a, b):
+        dist = np.linalg.norm(a - b)
+        return dist
+
     def closest(self, row, k):
-        best_dist = euc(row, self.X_train[0])
+        best_dist = self.euc(row, self.X_train[0])
         best_index = 0
         for i in range(self.k, len(self.X_train)):
-            dist = euc(row, self.X_train[i])
+            dist = self.euc(row, self.X_train[i])
             if dist < best_dist:
                 best_dist = dist
                 best_index = i
         return self.y_train[best_index]
 
-    def predict(self, X_test):
+    def predict(self, X_test, k):
         predictions = []
         for row in X_test:
-            label = self.closest(row, 1)
+            label = self.closest(row, k)
             predictions.append(label)
         return predictions
 
 
-def euc(a, b):
-    return distance.euclidean(a, b)
-
-
 def process_data():
     iris = datasets.load_iris()
-    X = iris.data
-    y = iris.target
+    X = np.array(iris.data)
+    y = np.array(iris.target)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
     return X_train, X_test, y_train, y_test
 
@@ -44,7 +44,7 @@ def knn(k_nieghbor):
     X_train, X_test, y_train, y_test = process_data()
     classifier = scrappy_knn()
     classifier.fit(X_train, y_train, k_nieghbor)
-    predictions = classifier.predict(X_test)
+    predictions = classifier.predict(X_test, k_nieghbor)
     print(accuracy_score(y_test, predictions))
 
 
