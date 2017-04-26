b0ae732eaa6fb6c33d64c6e4f4fea453f2bf6215
Yaseen Lotfi <yalotfi@outlook.com>
2017-04-26 16:31:48 -0400

    feature scaling method added

--- process_csv.py
+++ process_csv.py
@@ -4,7 +4,7 @@ from sklearn.cross_validation import train_test_split
 
 
 def normalize_df(df):
-    return (df - df.mean()) / (df.max() - df.min())
+    return (df - df.min()) / (df.max() - df.min())
 
 
 def process_csv(path, colnames):
@@ -23,12 +23,16 @@ def process_csv(path, colnames):
     labels = labels.factorize()[0]
 
     # Concatenate factorized and numeric data subsets
-    features = np.array(normalize_df(pd.concat([factors, numerics], axis=1)))
+    features = normalize_df(pd.concat([factors, numerics], axis=1))
+
+    # Split Data
     [X_train, X_test,
-     y_train, y_test] = train_test_split(features, labels, test_size=0.3)
+     y_train, y_test] = train_test_split(
+        np.array(features), labels, test_size=0.3)
+    headers = features.columns.values
 
     # Train and test sets for X and y
-    return X_train, X_test, y_train, y_test
+    return X_train, X_test, y_train, y_test, headers
 
 
 def main():
@@ -45,17 +49,25 @@ def main():
 
     # Process data with function from above
     [X_train, X_test,
-     y_train, y_test] = process_csv(path=file_path, colnames=names)
-    print(X_train.shape)
-    print(X_test.shape)
-    print(y_train.shape)
-    print(y_test.shape)
+     y_train, y_test,
+     headers] = process_csv(path=file_path, colnames=names)
+
+    # # Append labels to train and test for export
+    m_train = len(y_train)
+    m_test = len(y_test)
+    train = np.hstack((X_train, np.reshape(y_train, (m_train, 1))))
+    test = np.hstack((X_test, np.reshape(y_test, (m_test, 1))))
+
+    # print(X_train.shape)
+    # print(train.shape)
+    # print(X_test.shape)
+    # print(test.shape)
+    # print(headers)
 
     # Write csv files ready for analysis
-    np.savetxt('data/x_train.csv', X_train, delimiter=',')
-    np.savetxt('data/X_test.csv', X_test, delimiter=',')
-    np.savetxt('data/y_train.csv', y_train, delimiter=',')
-    np.savetxt('data/y_test.csv', y_test, delimiter=',')
+    np.savetxt('train.csv', train, delimiter=',')
+    np.savetxt('test.csv', test, delimiter=',')
+    np.savetxt('headers.csv', headers, delimiter=',', fmt='%s')
 
 
 if __name__ == '__main__':
